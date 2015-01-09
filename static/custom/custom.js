8f706709c951bdf7fcdfdda47e5c698f91db671b
Wusung Peng <twkoci@gmail.com>
2015-01-09 23:48:06 +0000

    Remove scroll above window

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -4,49 +4,90 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-// Notebook.prototype.find_cell_index = function (cell) {
-//     return 0;
-// };
+var afterModifyDom= false;
 
-var domIsModified = false;
+IPython.Notebook.prototype.execute_selected_cell = function (options) {
+    var default_options = {terminal: false, add_new: false};
+    $.extend(default_options, options);
+    var that = this;
+    var cell = that.get_selected_cell();
+    var cell_index = that.find_cell_index(cell);
+    if (cell instanceof IPython.CodeCell) {
+        cell.execute();
+    }
+    if (default_options.terminal) {
+        cell.select_all();
+    } else {
+        if ((cell_index === (that.ncells()-1)) && default_options.add_new) {
+            that.insert_cell_below('code');
+            // If we are adding a new cell at the end, scroll down to show it.
+            that.scroll_to_bottom();
+        } else {
+            that.select(cell_index+1);
+        };
+    };
+    this.set_dirty(true);
+};
 
 IPython.Notebook.prototype.get_cell_elements = function () {
-    // return $("#notebook-container").find(".cell").not('.cell .cell');
-    if (domIsModified)
+    if (afterModifyDom)
         return $("#notebook-container .active").find(".cell");
     else 
         return $("#notebook-container").find(".cell").not('.cell .cell');
 };
 
-// IPython.Notebook.prototype.get_selected_cell = function () {
-//     return $("#content-0 cell");
-// };
-
-$([IPython.events]).on('notebook_loaded.Notebook', function() {
-    
-    domIsModified = true;
-    
-    var html = '';
-
-    $('#notebook-container').append('<div class="tabbable" id="tabs_table">');
-    $('#tabs_table').append('<ul class="nav nav-tabs" id="nav_tabs"></ul>');
-    $('#tabs_table').append('<div class="tab-content" id="tab_content">');
-    
+$([IPython.events]).on('create.Cell', function(cell, index) {
     var i = 0;
-    
     $("#notebook-container .cell").each(function() {
-        
         var nav_id = 'nav-id_' + i;
         var content_id = 'content-' + i;
-        //$("#ipython-main-app").append('<div id="notebook_pane" class="border-box-sizing"></div>');
-        $("#nav_tabs").append('<li id="' + nav_id + '"><a href="#' + content_id + '" data-toggle="tab">Editor ' + i + '</a></li>');
+        $(this).appendTo($('#content-' + i));
         
-        $('#tab_content').append('<div class="tab-pane" id=content-' + i + '></div>');
+        i++;
+    });
+    
+    $('#tab-content .tab-pane').each(function() {
+        if ($(this).find('div.cell').size() > 1) {
+           
+        }
+    });
+
+});
+
+function render_editor() {
+    afterModifyDom = true;
+    $('#notebook-container').append('<div class="tabbable" id="tabs_table">');
+    $('#tabs_table').append('<ul class="nav nav-tabs" id="nav-tabs"></ul>');
+    $('#tabs_table').append('<div class="tab-content" id="tab-content">');
+    
+    var i = 0;
+    $("#notebook-container .cell").each(function() {
+        var nav_id = 'nav-id_' + i;
+        var content_id = 'content-' + i;
+        $("#nav-tabs").append('<li id="' + nav_id + '"><a href="#' + content_id + '" data-toggle="tab">Editor ' + i + '</a></li>');
+        $('#tab-content').append('<div class="tab-pane" id="content-' + i + '"></div>');
         $(this).appendTo($("#content-" + i));
         
         i++;
     });
     
+    $("#nav-tabs").append('<li id="new-editor"><a href="#" data-toggle="tab">+</a></li>');
+    $("#new-editor").click(function(e) {
+        var nextTab = $('#nav-tabs li').size()+1;
+      	$('#nav-tabs').append('<li id="nav-id_' + nextTab + '"><a href="#content-'+nextTab+'" data-toggle="tab">Editor '+nextTab+'</a></li>');
+      	$('#tab-content').append('<div class="tab-pane" id="content-'+nextTab+'">tab' +nextTab+' content</div>');
+      	$("#new-editor").appendTo('#nav-tabs');
+
+      	IPython.notebook.insert_cell_below('code');
+      	// make the new tab active
+      	// $('#nav-tabs a')[$('#nav-tabs a').size() - 2].tab('show');
+    });
+    
     $('.end_space').remove();
-    $('nav-id-0').addClass('active');
+    $('#nav-id_0').addClass('active');
+    $('#content-0').addClass('active');
+}
+
+$([IPython.events]).on('notebook_loaded.Notebook', function() {
+    render_editor();
 });
