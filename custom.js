694c53bb7782f6ef6f84b74bc32c1e166538b722
Wusung Peng <twkoci@gmail.com>
2015-01-27 11:03:42 +0800

    Include sidebar extension

--- custom.js
+++ custom.js
@@ -4,12 +4,10 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-
 require(["base/js/events"], function (events) {
-    
-    //IPython.load_extensions('module-sidebar/module-sidebar'); 
+
+    IPython.load_extensions('module-sidebar/module-sidebar'); 
     IPython.load_extensions('module-tabs/module-tabs'); 
     //IPython.load_extensions('module-tree/module-tree'); 
-
 });
 
