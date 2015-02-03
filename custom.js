a953b433fb520c3de797f78efbad297e4fb05c62
Wusung Peng <twkoci@gmail.com>
2015-02-03 21:17:14 +0800

    Add filetree

--- custom.js
+++ custom.js
@@ -4,6 +4,11 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+
+
+var utils = IPython.utils;
+
+
 require(["base/js/events"], function (events) {
 
     // IPython.load_extensions('module-sidebar/module-sidebar'); 
@@ -11,12 +16,75 @@ require(["base/js/events"], function (events) {
     //IPython.load_extensions('module-tree/module-tree'); 
 });
 
-require(["custom/module-tabs"], function (events) {
-	console.log('module-tabs loaded');
+require(["custom/module-tabs", 
+		"custom/module-sidebar", 
+		'custom/jquery.treeview'], 
+		function (events) {
+
+require(['custom/jquery.cookie'], function () {
+
+	'use strict';
+	console.log('custom module loaded');
+
+	$('#data.tab-pane').html('');
+
+   	var list_loaded = function (data, status, xhr, param) {
+    	var len = data.length;
+
+    	if (len > 0) {
+    		$('<div id="directory-tree" class="filetree"></div>')
+    			.appendTo('#directory.tab-pane');
+    	}
+    	for (var i=0; i<len; i++) {
+			var item = data[len];
+			if (data[i].type === 'directory') {
+				$('<li></li>').append('<span class="folder">' + data[i].name + '</span>')
+					.appendTo('#directory-tree.filetree');
+			} else {
+				$('<li></li>').append('<span class="file">' + data[i].name + '</span>')
+					.appendTo('#directory-tree.filetree');
+			}
+    	}
+
+    	$('#directory-tree.filetree').appendTo('#directory.tab-pane');
+
+    	$(document).ready(function(){
+			$("#directory-tree").treeview({
+				animated: "fast",
+				collapsed: true,
+				unique: true,
+				persist: "cookie",
+				toggle: function() {
+					window.console && console.log("%o was toggled", this);
+				},
+				'click.treeview': function () {
+					window.console && console.log("%o was toggled", this);
+				}
+			});    		
+    	})
+    	
+   	}
+
+    var settings = {
+        processData : false,
+        cache : false,
+        type : "GET",
+        dataType : "json",
+        success : $.proxy(list_loaded, this),
+        error : $.proxy( function(xhr, status, error){
+            utils.log_ajax_error(xhr, status, error);
+            list_loaded([], null, null, {msg:"Error connecting to server."});
+                         },this)
+    };
+
+    var url = utils.url_join_encode(
+                '/',
+                'api',
+                'notebooks'
+        );
+
+	$.ajax(url, settings);
+
 
 });
-
-require(["custom/module-sidebar"], function (events) {
-	console.log('module-sidebar loaded');
 });
-
