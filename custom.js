3b42159a9b1ef69eeb6be22658e09216555d58d8
Wusung Peng <twkoci@gmail.com>
2015-02-05 23:16:29 +0800

    Add jquery-contextMenu

--- custom.js
+++ custom.js
@@ -4,10 +4,7 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-
-
 var utils = IPython.utils;
-
 
 require(["base/js/events"], function (events) {
 
@@ -16,12 +13,21 @@ require(["base/js/events"], function (events) {
     //IPython.load_extensions('module-tree/module-tree'); 
 });
 
+
+var nbname = '';
+
+;(function () {
+
 require(["custom/module-tabs", 
-		"custom/module-sidebar", 
-		'custom/jquery.treeview'], 
+		"custom/module-sidebar",
+		'custom/jquery.cookie',
+		'custom/jquery.treeview',
+		'custom/jquery.ui.position',
+		'custom/jquery.contextMenu'
+		], 
 		function (events) {
 
-require(['custom/jquery.cookie'], function () {
+//require(['custom/jquery.cookie'], function () {
 
 	'use strict';
 	window.console && console.log('custom module loaded');
@@ -29,42 +35,104 @@ require(['custom/jquery.cookie'], function () {
 	$('#data.tab-pane').html('');
 
    	var list_loaded = function (data, status, xhr, param) {
+   		
     	var len = data.length;
 
     	if (len > 0) {
-    		$('<div id="directory-tree" class="filetree"></div>')
+    		$('<ul id="directory-tree" class="filetree"></ul>')
     			.appendTo('#directory.tab-pane');
     	}
     	for (var i=0; i<len; i++) {
 			var item = data[len];
 			if (data[i].type === 'directory') {
-				$('<li></li>').append('<span class="folder"><a href="' + data[i].name + '">' + data[i].name + '</a></span>')
-					.appendTo('#directory-tree.filetree');
+				$('<li class="context-menu-one box menu-1"></li>')
+					//.append('<span class="folder"><a href="' + data[i].name + '">' + data[i].name + '</a></span>')
+					.append('<span class="folder">' + data[i].name + '</span>' + 
+						'<ul><li><span class="placeholder">&nbsp;</span></li></ul>')
+					.appendTo('#directory-tree');
 			} else {
-				$('<li></li>').append('<span class="file"><a href="' + data[i].name + '">' + data[i].name + '</a></span>')
-					.appendTo('#directory-tree.filetree');
+				$('<li class=" context-menu-one box menu-1"></li>')
+					.append('<span class="file"><a href="' + data[i].name + '">' + data[i].name + '</a></span>')
+					//.append('<span class="file">' + data[i].name + '</span>')
+					.appendTo('#directory-tree');
 			}
     	}
-    	$('#directory-tree.filetree').appendTo('#directory.tab-pane');
-    	(function () {
-	    	$(function(){
-				$('#directory-tree.filetree').treeview({
-					animated: "fast",
-					collapsed: true,
-					unique: true,
-					persist: "cookie",
-					"toggle": function() {
-						window.console && console.log("%o was toggled", this);
-					},
-					'click.treeview': function () {
-						window.console && console.log("%o was toggled", this);
-					}
-				});    		
-	    	})    		
-    	}( jQuery ) );    	
 
+ //    	$('	<ul id="browser" class="filetree">' +
+	// 	'<li><span class="folder">Folder 1</span>' +
+	// 		'<ul>' +
+	// 			'<li><span class="folder">Item 1.1</span>' +
+	// 				'<ul>' +
+	// 					'<li><span class="file">Item 1.1.1</span></li>' +
+	// 				'</ul>' +
+	// 			'</li>' +
+	// 			'<li><span class="folder">Folder 2</span>' +
+	// 				'<ul>' +
+	// 					'<li><span class="folder">Subfolder 2.1</span>' +
+	// 						'<ul id="folder21">' +
+	// 							'<li><span class="file">File 2.1.1</span></li>' +
+	// 							'<li><span class="file">File 2.1.2</span></li>' +
+	// 						'</ul>' +
+	// 					'</li>' +
+	// 					'<li><span class="folder">Subfolder 2.2</span>' +
+	// 						'<ul>' +
+	// 							'<li><span class="file">File 2.2.1</span></li>' +
+	// 							'<li><span class="file">File 2.2.2</span></li>' +
+	// 						'</ul>' +
+	// 					'</li>' +
+	// 				'</ul>' +
+	// 			'</li>' +
+	// 			'<li class="closed"><span class="folder">Folder 3 (closed at start)</span>' +
+	// 				'<ul>' +
+	// 					'<li><span class="file">File 3.1</span></li>' +
+	// 				'</ul>' +
+	// 			'</li>' +
+	// 			'<li><span class="file">File 4</span></li>' +
+	// 		'</ul>' +
+	// 	'</li>' +
+	// '</ul>').appendTo('#directory-tree');
+    	//$('#directory-tree.filetree').appendTo('#directory.tab-pane');
     	$('#directory').tab('show');
-   	}
+    	$('#directory-tree .file a').click(function () {
+
+    	});
+
+		$('#directory-tree').treeview({
+			animated: "fast",
+			collapsed: true,
+			unique: true,
+			persist: "cookie",
+			"toggle": function() {
+				
+			},
+			'click.treeview': function () {
+				window.console && console.log("%o was toggled", this);
+			}
+		});
+
+		$.contextMenu({
+	        selector: '.context-menu-one', 
+	        callback: function(key, options) {
+	            var m = "clicked: " + key;
+	            window.console && console.log(m);
+				if (key == 'delete') {
+					nbname = $(this).find('a').html();
+					delete_notebook();
+		        } else if (key == 'add') {
+		        	new_notebook();
+		        }	            
+	        },
+	        items: {
+	            "delete": {name: "Delete", icon: "delete"},
+	            "add": {name: "Add", icon: "add"}
+	        }
+	    });
+	    
+	    $('.context-menu-one').on('click', function(e){
+	        console.log('clicked', this);
+	        
+	    })
+   	};
 
     var settings = {
         processData : false,
@@ -86,6 +154,85 @@ require(['custom/jquery.cookie'], function () {
 
 	$.ajax(url, settings);
 
+	var delete_notebook = function () {
 
+		var message = 'Are you sure you want to permanently delete the notebook: ' + nbname + '?';
+		IPython.dialog.modal({
+			title : "Delete notebook",
+			body : message,
+			buttons : {
+				Delete: {
+					class: "btn-danger",
+					click: function () {
+						var settings = {
+				            processData : false,
+				            cache : false,
+				            type : "DELETE",
+				            dataType : "json",
+				            success : function (data, status, xhr) {
+				                //parent_item.remove();
+				                $('a[href="' + nbname + '"').parent().parent().remove();
+				            },
+				            error : utils.log_ajax_error,
+				        };
+				        var url = utils.url_join_encode(
+				            '/',
+				            'api/notebooks',
+				            "",
+				            nbname
+				        );
+				        $.ajax(url, settings);
+					}
+				},
+				Cancel: {}
+			}
+		});
+
+
+	}
+
+	var new_notebook = function(){
+        var path = '';
+        var base_url = '/';
+        var settings = {
+            processData : false,
+            cache : false,
+            type : "POST",
+            dataType : "json",
+            async : false,
+            success : function (data, status, xhr) {
+                var notebook_name = data.name;
+                window.location.replace(utils.url_join_encode(
+                        base_url,
+                        'notebooks',
+                        path,
+                        notebook_name));
+            },
+            error : $.proxy(new_notebook_failed, this),
+        };
+        var url = utils.url_join_encode(
+            base_url,
+            'api/notebooks',
+            path
+        );
+        $.ajax(url, settings);
+    };
+
+    var new_notebook_failed = function (xhr, status, error) {
+        utils.log_ajax_error(xhr, status, error);
+        var msg;
+        if (xhr.responseJSON && xhr.responseJSON.message) {
+            msg = xhr.responseJSON.message;
+        } else {
+            msg = xhr.statusText;
+        }
+        IPython.dialog.modal({
+            title : 'Creating Notebook Failed',
+            body : "The error was: " + msg,
+            buttons : {'OK' : {'class' : 'btn-primary'}}
+        });
+    }
+//});
 });
-});
+
+}).call(this);
