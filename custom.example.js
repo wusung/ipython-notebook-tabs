3d41b96d847f33f7a0eaabd4134051f7f2163308
96003 <twkoci@gmail.com>
2015-03-09 17:27:39 +0800

    Migrate IPython 3

--- custom.example.js
+++ custom.example.js
@@ -6,17 +6,9 @@
 
 var utils = IPython.utils;
 
-require(["base/js/events"], function (events) {
-
-    // IPython.load_extensions('module-sidebar/module-sidebar'); 
-    // IPython.load_extensions('module-tabs/module-tabs'); 
-    //IPython.load_extensions('module-tree/module-tree'); 
-});
-
-
-require(["../nbextensions/dir-tabs/module-directory", 
+require([//"../nbextensions/dir-tabs/module-directory", 
 		'../nbextensions/dir-tabs/module-tabs'
 		],
-	function (events) {
+	function () {
 
 });
