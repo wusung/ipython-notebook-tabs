aba21d4250bd25938a39c0357353141062641087
Wusung Peng <twkoci@gmail.com>
2015-04-07 18:01:23 +0800

    Change require/define to IP3.

--- custom.example.js
+++ custom.example.js
@@ -4,11 +4,10 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-var utils = IPython.utils;
 
-require([//"../nbextensions/dir-tabs/module-directory", 
-		'../nbextensions/dir-tabs/module-tabs'
-		],
-	function () {
+define(function (require) {
+	"use strict";
 
+	var moduleTabs = require('../nbextensions/dir-tabs/module-tabs');
+	//var moduleDirectory = require('../nbextensions/dir-directory/module-directory');
 });
